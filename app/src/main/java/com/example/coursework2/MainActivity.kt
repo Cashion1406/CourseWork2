ed05bdfb63a139e4ff8bf57cccaaf67a16ee1907
Cashion <cashion1406@gmail.com>
2022-11-10 01:52:23 +0700

    done fertch image + camera

--- app/src/main/java/com/example/coursework2/MainActivity.kt
+++ app/src/main/java/com/example/coursework2/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.coursework2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.webkit.URLUtil
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_mainfragment.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
 
-        ed_image_url.addTextChangedListener {
-
-            if (ed_image_url.text!!.isEmpty()){
-
-                return@addTextChangedListener
-            }
-            Picasso.get().load(ed_image_url.text.toString()).into(imageView);
-        }
     }
+
+
 }
